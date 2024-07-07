1900c60ffb95482157bbe1104247f57f506d07df
n-whittaker <64340124+n-whittaker@users.noreply.github.com>
2024-07-07 18:04:34 +0100

    Add read button change and rating disabled

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const submit = document.querySelector(".submit");
+const form = document.querySelector(".form");
 const newTitleInput = document.querySelector(".title-input");
 const newAuthorInput = document.querySelector(".author-input");
 const newPagesInput = document.querySelector(".pages-input");
-const newReadStatusInput = document.querySelector(".read-input");
+const newReadStatusInput = document.querySelector(".status-check");
 const newRatingInput = document.querySelector(".rating-input");
+
+let ratingNum = document.querySelector(".rating-number")
 
 function Book(title, author, pages, status, rating) {
         this.title = title;
@@ -14,13 +16,13 @@ function Book(title, author, pages, status, rating) {
 }
 
 
-submit.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const title = newTitleInput.value;
     const author = newAuthorInput.value;
     const pages = newPagesInput.value;
-    const status = newReadStatusInput.value;
+    const status = newReadStatusInput.checked;
     const rating = newRatingInput.value;
 
     const newBook = new Book(title, author, pages, status, rating);
@@ -32,22 +34,84 @@ submit.addEventListener('click', (event) => {
 });
 
 
-function addNewBook(newBookObject) {
+function addNewBook(newBook) {
     const bookShelf = document.querySelector(".books");
-    const newBook = newBookObject;
+
+    const statusClass = newBook.status === true ? "read" : "unread";
+    const statusText = newBook.status === true ? "Read" : "Unread";
 
     bookShelf.innerHTML += `
         <div class="book-card">
             <h2 class="book-card-title">${newBook.title}</h2>
             <h3 class="book-card-author">${newBook.author}</h3>
             <p class="book-card-pages">${newBook.pages}</p>
-            <button class="book-card-read">${newBook.status}</button>
+            
+ 
+            <button class="book-card-read ${statusClass}">${statusText}</button>
+
             <label>
-                <input type="number" class="book-card-rating" placeholder="None" min="0" max="5" value="${newBook.rating}">
+                <input type="number" class="book-card-rating" placeholder="None" min="0" max="5" value="${newReadStatusInput.checked ? newBook.rating : undefined}">
             </label>
             <button class="remove-btn">Delete</button>
         </div>
-    
+        
     `;
+
+    form.reset();
+    ratingNum.textContent = "Slide to rate";
+    newRatingInput.disabled = true;
+    ratingNum.style.color = "#b7b7b7";
+    attachedStatusEventListeners(); // This makes sure all, including the new book, have event listeners on their read buttons.
+
+
 }
+
+
+newRatingInput.addEventListener('change', () => {
+    ratingNum.textContent = newRatingInput.value.toString();
+})
+
+
+newReadStatusInput.addEventListener('change', () => {
+    newRatingInput.disabled = !newReadStatusInput.checked;
+
+    if (newReadStatusInput.checked) {
+        ratingNum.style.color = "black";
+        ratingNum.textContent = newRatingInput.value.toString();
+    } else {
+        ratingNum.style.color = "#b7b7b7";
+        ratingNum.textContent = "Slide to rate";
+
+    }
+})
+
+
+
+function attachedStatusEventListeners() {
+    const statusButtons = document.querySelectorAll(".book-card-read");
+    // query selector all creates a list
+
+
+    // Have to iterate over that list, giving each button its own listener
+    statusButtons.forEach(btn => { //for each will run through the list with a function within the brackets
+        btn.addEventListener('click', () => {
+
+            if (btn.textContent === "Unread") {
+                btn.style.backgroundColor = '#c3e8c3';
+                btn.textContent = 'Read'
+            } else {
+                btn.style.backgroundColor = '#cecece';
+                btn.textContent = 'Unread'
+            }
+
+
+
+        })
+    })
+
+}
+
+
+attachedStatusEventListeners(); // this gives all the buttons their initial event listener
+
 
